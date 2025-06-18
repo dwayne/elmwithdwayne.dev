c03187a3bef1cc434fc00821552ba27d5a9d6528
Dwayne Crooks <me@dwaynecrooks.com>
2025-06-18 07:35:41 -0400

    Switch to Astro

---
+++ README.md
@@ -0,0 +1,3 @@
+# elmwithdwayne.dev
+
+The website source code for [Elm with Dwayne](https://elmwithdwayne.dev/).
---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#
+# Usage: . init.sh
+#
+
+alias d='pnpm dev'
+alias b='pnpm build'
+alias p='pnpm preview'
