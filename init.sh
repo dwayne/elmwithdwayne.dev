759489510609361a759559c7659aa97b72e5e9c7
Dwayne Crooks <me@dwaynecrooks.com>
2025-06-28 06:11:33 -0400

    Rework the production deploy script

--- init.sh
+++ init.sh
@@ -4,6 +4,24 @@
 # Usage: . init.sh
 #
 
+dist="${dist:?}"
+
+
+# FUNCTIONS
+
+
+deploy-production () {
+  pnpm build
+  deploy "$@" "$dist" release/production
+}
+
+export -f deploy-production
+
+
+# ALIASES
+
+
 alias d='pnpm dev'
 alias b='pnpm build'
 alias p='pnpm preview'
+alias dp=deploy-production
